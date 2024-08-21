29608f0444eeb4b3550d29808b454e26ac1c8701
Hardik-Dulani <hardikdulani66@gmail.com>
2024-08-21 21:47:49 +0530

    Classification complete

--- App.py
+++ App.py
@@ -37,13 +37,14 @@ st.title("fílos AI Chat")
 # Get user input
 user_input = st.text_input("Enter your message:",key = 'base_prompt')
 
+
 if user_input:
     # Send the user input to the model and get the response
-    response = chat_session.send_message(user_input)
+    response = chat_session.send_message(f"You are my helper AI, that supports prompts for other AI in my application, I have multiple AIs that work on certain domains, You just have to take in the prompt I provide, understand and classify it in one of following categories, ['Continuous', 'Image generation', 'Rewrite', 'Summarize' , 'Internet Search', 'General', 'Immoral'] you just have to give one word output with the name of the category, nothing else, regardless of the prompt, please make sure there are no other characters or line break prompt:{user_input}")
     
     # Display the response
-    st.write(response.text)
-
+    
+    st.text(response)
     if st.button("Go Back to Home"):
                     st.session_state['redirect'] = True
                     st.experimental_rerun()
